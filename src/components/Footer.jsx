044eb5f4185e39afc09a896708db6676260e9ee5
Semeena Hiba <semeenahiba@gmail.com>
2024-08-02 09:51:30 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,78 +1,61 @@
-import { faFacebook, faInstagram, faPinterest } from '@fortawesome/free-brands-svg-icons';
-import { faRing } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { faFacebook, faInstagram, faLinkedinIn, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function Footer() {
   return (
     <>
-      <div className="container-fluid bg-tertiary p-5">
-        <div className="row">
-          <div className="col-md-2">
-            <h4 className='text-dark Bold'> 
-              <FontAwesomeIcon icon={faRing} className="me-2 mt-5" style={{fontWeight: 'bold', fontSize: '1.85rem'}} />
-              Wedding Bells
-            </h4>
+    <div className='row w-100 p-5 bg-dark'>
+            <div className="col-md-4" >
+            <h4 className='text-warning mb-4'><FontAwesomeIcon icon={faHeart} style={{color: "warning"}} />About Us</h4>
+            <p className='text-warning'style={{ textAlign:'justify' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                 standard dummy text ever since the 1500s, when an unknown printer took a galley
+                 of type and scrambled it to make a type specimen book. It has survived not only five centuries, </p>
+                 </div> 
+            <div className="col-md-2  d-md-flex justify-content-center" >
+             <div>
+              <h4 className='mb-4 text-warning'>Links</h4>
+              <div >
+             <Link to={'/'}> <p className='text-warning'>User</p></Link>
+              <Link to={'/home'}><p className='text-warning'>Vendor</p></Link>
+              <Link to={'/viewcatering'}><p className='text-warning'>Catering</p></Link>
+              <Link to={'/viewvenue'}><p className='text-warning'>Venue</p></Link>
           </div>
-          <div className="col-md-2 d-flex justify-content-center align-items-center flex-column">
+            </div>
+              </div>
+            <div className='col-md-1'></div>
+            <div className="col-md-2" >
             <div>
-              <h3 className='text-dark Bold' style={{ marginTop: '-39px', fontWeight: 'bold', fontSize: '1.65rem' }}>Company</h3>
-              <p className='mt-3 text-dark'>About Us</p>
-              <p className='text-dark'>Services</p>
-              <p className='text-dark'>Blog</p>
+              <h4 className='mb-4 text-warning'>Guides</h4>
+              <div>
+              <p className='text-warning'>React</p>
+              <p className='text-warning'>React Bootstrap</p>
+              <p className='text-warning'>Bootswatch</p>
             </div>
-          </div>
-          <div className="col-md-1 d-flex justify-content-center align-items-center flex-column">
-            <div>
-              <h3 className='text-dark Bold' style={{ marginTop: '-39px', fontWeight: 'bold', fontSize: '1.65rem' }}>Support</h3>
-              <p className='mt-3 text-dark'>Contact Us</p>
-              <p className='text-dark'>Faq</p>
-              <p className='text-dark'>Privacy Policy</p>
             </div>
-          </div>
-          <div className="col-md-3 d-flex justify-content-center align-items-center flex-column">
-            <div >
-              <h3 className='text-dark Bold' style={{ marginTop: '-79px', fontWeight: 'bold', fontSize: '1.65rem' }}>Quick Links</h3>
-              <p className='mt-3 text-dark'>Testimonials</p>
-              <p className='text-dark'>Vendor Registration</p>
             </div>
-          </div>
-          <div className="col-md-3">
-            <div className='mt-3'>
-              <p className='text-dark text-center' style={{ marginTop: '-50px', fontWeight: 'bold', fontSize: '1.65rem', whiteSpace: 'nowrap' }}>Subscribe to Our Newsletter</p>
-              <p>Stay updated with the latest wedding planning tips and trends.<br />Enter your email to subscribe</p>
-              <div className='d-flex '>
-                <input type="text" placeholder='Email Id' className='form-control' />
-                <button className='btn btn-danger ms-4 ' style={{ backgroundColor: 'red' }}>Subscribe</button>
+            <div className="col-md-3 d-md-flex justify-content-center" >
+                <div>
+              <h4 className='text-warning'>Contact us</h4>
+              <div className='d-flex mt-4'>
+                <input type='text'className='form-control text-bg-warning'placeholder='Email Id'/>
+                <button className='btn btn-warning ms-4'>Subscribe</button>
+              </div>
+              <div className='d-flex justify-content-between mt-4'>
+<FontAwesomeIcon icon={faInstagram} className='fa-2x text-warning'/>
+<FontAwesomeIcon icon={faTwitter}className='fa-2x text-warning'/>
+<FontAwesomeIcon icon={faFacebook}className='fa-2x text-warning'/>
+<FontAwesomeIcon icon={faLinkedinIn}className='fa-2x text-warning'/>
+<FontAwesomeIcon icon={faWhatsapp}className='fa-2x text-warning'/>
               </div>
             </div>
-          </div>
-        </div>
-        <hr className="my-4 " style={{ marginTop: '20px', color: 'black' }} />
-        <div className="row ">
-          <div className="col-md-3">
-            <p> <span style={{ color: 'black', fontWeight: 'bold' }}>© 2024 wedding Bells.</span> All Rights Reserved.<span style={{ fontStyle: 'italic' }}>Terms of Service and Privacy</span></p>
-          </div>
-          <div className="col-md-2">
-            <p>Terms of service</p>
-          </div>
-          <div className="col-md-2">
-            <p>Privacy Policy</p>
-          </div>
-          <div className="col-md-4 d-flex justify-content-end align-items-center">
-            <div className="icon-container me-2 bg-dark">
-              <FontAwesomeIcon icon={faFacebook} style={{ fontSize: '1rem', color: 'white', border: '2px solid black', borderRadius: '0', padding: '3px' }} />
-            </div>
-            <div className="icon-container me-2 bg-dark">
-              <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '1rem', color: 'white', border: '2px solid black', borderRadius: '0', padding: '3px' }} />
-            </div>
-            <div className="icon-container bg-dark">
-              <FontAwesomeIcon icon={faPinterest} style={{ fontSize: '1rem', color: 'white', border: '2px solid black', borderRadius: '0', padding: '3px' }} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
+    </div>
+
+     </>
   );
 }
 
