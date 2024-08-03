52f3f5e42002f32014280b6de57f6d5dbd007b18
linnetjohn3@gmail.com <linnetjohn3@gmail.com>
2024-08-03 20:30:27 +0530

    changed

--- src/pages/HomeUser.jsx
+++ src/pages/HomeUser.jsx
@@ -10,35 +10,37 @@ function HomeUser() {
      
 
 
-      <section className="search-section">
-      <div className="search-container ">
-        <h2 className="text-center" style={{ fontSize: '40px', fontWeight: '600',marginTop:'-100px' }}>
-          <span style={{color: 'white' }}>Make Your</span> <br /> 
-          <span style={{ color: '#d63384' }}>Dream Wedding </span> <span style={{ color: 'white' }}>a Reality </span> 
+     <section className="search-section" style={{ position: 'relative' }}>
+      <div className="search-container" style={{ padding: '50px 0' }}>
+        <h2 className="text-center" style={{ fontSize: '40px', fontWeight: '600', color: 'white', position: 'relative', zIndex: '1' }}>
+          <span style={{ color: 'white' }}>Make Your</span> <br />
+          <span style={{ color: '#d63384' }}>Dream Wedding </span> <span style={{ color: 'white' }}>a Reality </span>
         </h2>
-        <p className="justify-content-center align-items-center d-flex" style={{ textAlign: 'justify',alignItems:'center' }}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti <br/>natus sequi esse ex quos qui
-          amet omnis quisquam labore magnam. <br/>Totam fugit natus odio laudantium? 
+        <p className="justify-content-center align-items-center d-flex" style={{ textAlign: 'justify', alignItems: 'center', color: 'white', position: 'relative', zIndex: '1' }}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti <br />natus sequi esse ex quos qui
+          amet omnis quisquam labore magnam. <br />Totam fugit natus odio laudantium? 
           Ab, impedit! Cumque, inventore veniam.
         </p>
-        <div className="d-flex justify-content-center align-items-center  section">
-          <Dropdown className="dropdown-container">
-            <Dropdown.Toggle variant="white border rounded-0" id="dropdown-basic">
-              Category
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1"><Link to={'viewvenue'}>Venues</Link></Dropdown.Item>
-              <Dropdown.Item href="#/action-2"><Link to={'viewcatering'}>Catering</Link></Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <div className="input-container justify-content-around">
-            <input type="text" placeholder="Location" className="text-center bg-transparent p-2 border-white" />
-            <button className='btn btn-danger ms-4  p-2' style={{width:'100px'}}>Search</button>
+        <div className="row justify-content-center align-items-center section" style={{ position: 'relative', zIndex: '1' }}>
+          <div className="col-12 col-md-auto mb-2 mb-md-0">
+            <Dropdown className="dropdown-container">
+              <Dropdown.Toggle variant="white border rounded-0" id="dropdown-basic">
+                Category
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1"><Link to={'viewvenue'}>Venues</Link></Dropdown.Item>
+                <Dropdown.Item href="#/action-2"><Link to={'viewcatering'}>Catering</Link></Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <div className="col-12 col-md-auto d-flex align-items-center">
+            <input type="text" placeholder="Location" className="form-control text-center bg-transparent p-2 border-white me-2" />
+            <button className="btn btn-danger p-2" style={{ width: '100px' }}>Search</button>
           </div>
         </div>
       </div>
     </section>
+
 
 
 
