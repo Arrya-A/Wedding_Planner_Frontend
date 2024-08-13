b9ccb7b5c2be9457637e2d9eb5d38dc7cbc03f2b
RincyRaveendran <rincyjiyash@gmail.com>
2024-08-13 23:57:13 +0530

    date added

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import { Col, Row } from 'react-bootstrap';
-
+import { Calendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function VenueCard({ venue }) {
   return (
@@ -36,6 +37,20 @@ function VenueCard({ venue }) {
             
             <Col md={12} className='mb-3'>
               <Card.Text>Price:{venue?.price}</Card.Text>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={12} className='mb-3'>
+              <Card.Text>Booked Dates:</Card.Text>
+              <Calendar
+                tileClassName={({ date, view }) => {
+                  if (venue.bookedDates?.find(d => new Date(d).toDateString() === date.toDateString())) {
+                    return 'booked';
+                  }
+                  return null;
+                }}
+              />
             </Col>
           </Row>
         </Card.Body>
